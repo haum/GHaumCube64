5446d55d4e78165700c1c57280e3292db25ca948
JackDesBwa <jackdesbwa@gmail.com>
2021-11-09 20:53:58 +0100

    Add TCP stream to get buttons and control leds for the 24hc21 challenge

--- code/remote/24hc21_challenge/24hc21_challenge.ino
+++ code/remote/24hc21_challenge/24hc21_challenge.ino
@@ -8,13 +8,13 @@
 
 AppConfig config;
 ESP8266WebServer webserver(80);
+WiFiServer tcpbtns_server(540);
+WiFiClient tcpbtns_client;
 AppHttp http_handler(config, webserver);
 DNSServer dns;
 bool connect_enabled;
 bool ap_enabled;
 bool apfallback_enabled;
-
-const int leds[] = {D0, D3, D4};
 int led_nb = 0;
 
 void setup() {
@@ -89,11 +89,15 @@ void setup() {
   }
 
   MDNS.begin(config.hostname.value());
+  tcpbtns_server.begin();
 }
 
+int buttons = 0;
+int leds = -1;
 bool connectionFailed = false;
 int connectCounter = 0;
-long next_change = 0;
+long next_change_led = 0;
+long next_change_btns = 0;
 void loop() {
   ota_loop();
   dns.processNextRequest();
@@ -101,33 +105,64 @@ void loop() {
 
   if (WiFi.status() == WL_CONNECTED) {
     long time = millis();
-    if (time > next_change) {
-      next_change = time + 200;
+    if (time > next_change_led) {
+      next_change_led = time + 200;
       led_nb = (led_nb + 1) % 3;
-      digitalWrite(D0, HIGH);
-      digitalWrite(D3, HIGH);
-      digitalWrite(D4, HIGH);
-      digitalWrite(leds[led_nb], LOW);
+      if (leds < 0 || leds > 7) {
+        digitalWrite(D0, led_nb != 0);
+        digitalWrite(D3, led_nb != 1);
+        digitalWrite(D4, led_nb != 2);
+      } else {
+        digitalWrite(D0, (leds & 1) == 0);
+        digitalWrite(D3, (leds & 2) == 0);
+        digitalWrite(D4, (leds & 4) == 0);
+      }
     }
   }
 
-  digitalWrite(D2, LOW);
-  if (!digitalRead(D6)) Serial.println("X+");
-  if (!digitalRead(D5)) Serial.println("Y+");
-  if (!digitalRead(D7)) Serial.println("Z+");
-  digitalWrite(D2, HIGH);
+  WiFiClient client = tcpbtns_server.available();
+  if (client) {
+    tcpbtns_client.stop();
+    tcpbtns_client = client;
+    tcpbtns_client.println("24HC21 remote");
+    leds = 0;
+  }
 
-  digitalWrite(D1, LOW);
-  if (!digitalRead(D6)) Serial.println("X-");
-  if (!digitalRead(D5)) Serial.println("Y-");
-  if (!digitalRead(D7)) Serial.println("Z-");
-  digitalWrite(D1, HIGH);
+  if (tcpbtns_client.available()) {
+    leds = tcpbtns_client.parseInt();
+    long toread = tcpbtns_client.available();
+    for (int i = 0; i < toread; ++i) tcpbtns_client.read();
+  }
+  if (!tcpbtns_client.connected()) leds = -1;
 
-  digitalWrite(D8, LOW);
-  if (!digitalRead(D6)) Serial.println("✗");
-  if (!digitalRead(D5)) Serial.println("•");
-  if (!digitalRead(D7)) Serial.println("✓");
-  digitalWrite(D8, HIGH);
+  long time = millis();
+  if (time > next_change_btns) {
+    next_change_btns = time + 50;
+
+    int old = buttons;
+    buttons = 0;
+
+    digitalWrite(D2, LOW);
+    buttons |= !digitalRead(D6) ? 1 << 0 : 0;
+    buttons |= !digitalRead(D5) ? 1 << 1 : 0;
+    buttons |= !digitalRead(D7) ? 1 << 2 : 0;
+    digitalWrite(D2, HIGH);
+
+    digitalWrite(D1, LOW);
+    buttons |= !digitalRead(D6) ? 1 << 3 : 0;
+    buttons |= !digitalRead(D5) ? 1 << 4 : 0;
+    buttons |= !digitalRead(D7) ? 1 << 5 : 0;
+    digitalWrite(D1, HIGH);
+
+    digitalWrite(D8, LOW);
+    buttons |= !digitalRead(D6) ? 1 << 6 : 0;
+    buttons |= !digitalRead(D5) ? 1 << 7 : 0;
+    buttons |= !digitalRead(D7) ? 1 << 8 : 0;
+    digitalWrite(D8, HIGH);
+
+    if (old != buttons)
+      tcpbtns_client.println(buttons);
+  }
 
   // Connection indicator
   if (connect_enabled && !connectionFailed) {
@@ -142,11 +177,11 @@ void loop() {
         connectionFailed = true;
       } else {
         long time = millis();
-        if (time > next_change) {
+        if (time > next_change_led) {
           digitalWrite(D0, !digitalRead(D0));
           digitalWrite(D3, !digitalRead(D3));
           digitalWrite(D4, !digitalRead(D4));
-          next_change = time + 200;
+          next_change_led = time + 200;
           connectCounter++;
         }
       }
