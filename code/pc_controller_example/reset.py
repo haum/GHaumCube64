efc47dce9a9ee6c935455bdbc105e635cc3a6ac3
JbLb <jerome@jblb.net>
2021-11-17 16:10:01 +0100

    control sending thread before closing software

--- code/pc_controller_example/reset.py
+++ code/pc_controller_example/reset.py
@@ -2,9 +2,12 @@
 # -*- coding:utf8 -*-
 
 import time
+import threading
 
 from controller import TalController
 
 C = TalController(Ntal=64, Nled=3, source_name="HAUM Resetter")
 C.fill()
 C.stop()
+for thread in threading.enumerate():
+    if thread != threading.current_thread(): thread.join()
